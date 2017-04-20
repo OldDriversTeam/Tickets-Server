15c43a6f53e61f57d94ecadc06c5b59e68b94020
hshhuan <kegebo.h@gmail.com>
2017-04-20 13:40:06 +0800

    add .travis.yml

---
+++ src/test/java/test/MyTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MyTest {
+
+	@Test
+	public void simpleTest() {
+		assertTrue(true);
+	}
+
+}
